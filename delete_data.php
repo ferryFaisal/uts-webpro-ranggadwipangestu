a31734e5ab40d1fb38f19dde20a4a29761deb20e
ranggadwipangestu <105685457+ranggadwipangestu@users.noreply.github.com>
2022-10-28 10:57:11 +0700

    berhasil meng delete data dengan confirm

---
+++ delete_data.php
@@ -0,0 +1,15 @@
+<?php
+require 'database.php';
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM products WHERE id = '$id'";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Data deleted successfully";
+    header('Location: read_data.php');
+} else {
+    echo "Error deleting record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
